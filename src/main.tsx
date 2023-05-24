f82a00e83b9f7961828167c75321ff7412a73ec7
lucasoneves <casluhc@gmail.com>
2023-05-24 18:57:35 -0300

    Created a sass mixin for container

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
-import './assets/css/index.css'
+import './assets/css/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
