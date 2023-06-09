204a14e91b8583df4343355cd1716339b39610d8
lucasoneves <casluhc@gmail.com>
2023-06-09 19:10:23 -0300

    Styles and refactoring of footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import styles from "./Footer.module.scss";
+import navData from "../../data/nav";
+
+type Navigation = {
+  title: string;
+  path: string;
+};
+
 export const Footer = () => {
-  return <footer><h2>CookVegan</h2><span>Todos os direitos reservados - 2023</span></footer>
-}
+  const [menu, _] = useState<Navigation[]>(navData);
+  return (
+    <footer className={styles["footer"]}>
+      <div className={styles['container']}>
+        <div className={styles['sitemap']}>
+          <h2>CookVegan</h2>
+          <nav>
+            <ul>
+              {menu.map((item) => (
+                <li>{item.title}</li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        <span>Todos os direitos reservados - 2023</span>
+      </div>
+    </footer>
+  );
+};
