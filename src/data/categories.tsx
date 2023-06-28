c4b7db4ca547424f46d4b92add0114e3d9ad5190
lucasoneves <casluhc@gmail.com>
2023-06-28 14:00:29 -0300

    Refactoring categories cards

---
+++ src/data/categories.tsx
@@ -0,0 +1,48 @@
+import meals from '@/assets/images/thumb-meals.jpg'
+import cake from '@/assets/images/thumb-cake.jpg'
+import protein from '@/assets/images/thumb-protein.jpg'
+
+export const categoriesData = [
+  {
+    name: "Salads",
+    thumb: meals,
+    id: '69614742-15c9-11ee-be56-0242ac120002',
+    path: '/salads'
+  },
+  {
+    name: "Cakes",
+    thumb: cake,
+    id: '705e6e1c-15c9-11ee-be56-0242ac120002',
+    path: '/cakes'
+  },
+  {
+    name: "Protein",
+    thumb: protein,
+    id: '/protein',
+    path: '/protein'
+  },
+  {
+    name: "Meals",
+    thumb: meals,
+    id: '838d019c-15c9-11ee-be56-0242ac120002',
+    path: '/snacks'
+  },
+  {
+    name: "Desserts",
+    thumb: "@/assets/images/thumb-meals.jpg",
+    id: '890e01e8-15c9-11ee-be56-0242ac120002',
+    path: "/desserts"
+  },
+  {
+    name: "Snacks",
+    thumb: "@/assets/images/thumb-meals.jpg",
+    id: '8ee2cf5e-15c9-11ee-be56-0242ac120002',
+    path: '/snacks'
+  },
+  {
+    name: "Drinks",
+    thumb: "@/assets/images/thumb-meals.jpg",
+    id: '8ee2cf5e-15c9-11ee-be56-0242ac120002',
+    path: '/drinks'
+  }
+]
