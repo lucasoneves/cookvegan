284fa1c3c5bee8f33ba4bf28bcacda97b685e624
lucasoneves <casluhc@gmail.com>
2023-07-05 19:05:51 -0300

    WIP: Image url builder

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,18 +1,41 @@
 import styles from "./Category.module.scss";
-// import mealImage from '@/assets/images/thumb-meals.jpg';
-import { categoriesData } from "../../data/categories";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getCategories, client } from "../../lib/sanity.ts"
+
+type CategoryTypes = {
+  path: string;
+  name: string;
+  thumb: {
+    _type: string;
+    asset: {
+      _ref: string;
+      _type: string;
+    };
+  };
+};
 
 export const Category = () => {
-  const [categories, _] = useState(categoriesData);
+  useEffect(() => {
+    getCategoryData();
+  }, []);
+
+  async function getCategoryData() {
+    const data = await getCategories();
+    setCategories(data);
+  }
+  const [categories, setCategories] = useState<CategoryTypes[]>([]);
   return (
     <section className={styles["category"]}>
       <div className="container">
         <h2 className={styles["title"]}>Categories</h2>
         <ul className={styles["category-list"]}>
           {categories?.map((category) => (
-            <li key={category.id} className={styles["category-list--item"]}>
-              <img src={category.thumb} alt={category.thumb} className={styles['category-list--thumb']} />
+            <li key={category.path} className={styles["category-list--item"]}>
+              <img
+                src={category.thumb.asset._ref}
+                alt={category.name}
+                className={styles["category-list--thumb"]}
+              />
               <div className={styles["link"]}>
                 <a href={category.path}>{category.name}</a>
               </div>
@@ -23,3 +46,5 @@ export const Category = () => {
     </section>
   );
 };
+
+export default Category;
