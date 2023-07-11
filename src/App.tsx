4ffa7899290609b63928e456ab1f36b5955c21ea
lucasoneves <casluhc@gmail.com>
2023-07-11 19:02:03 -0300

    Refactoring of category component

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,52 @@ import { Hero } from "./components/Hero";
 import { RecipeList } from "./components/RecipeList";
 import { SearchBar } from "./components/SearchBar";
 import { SectionWhyVegan } from "./components/SectionWhyVegan";
+import { useEffect, useState } from "react";
+import { getCategories } from "./lib/sanity";
+import { CategoryTypes } from "./components/Category";
+import categoriesStyle from "./components/Category/Category.module.scss";
 
 function App() {
+  useEffect(() => {
+    getCategoryData();
+  }, []);
+  async function getCategoryData() {
+    const data = await getCategories();
+    setCategories(data);
+  }
+
+  const [categories, setCategories] = useState<CategoryTypes[]>([]);
+
   return (
     <>
       <Header logo="Cook Vegan" />
       <Hero>
-        <h2>Try new flavors through <strong>vegan recipes</strong></h2>
+        <h2>
+          Try new flavors through <strong>vegan recipes</strong>
+        </h2>
         <SearchBar title="Look for a recipe"></SearchBar>
       </Hero>
       <div className="container">
         <RecipeList titleList="Recently added:" />
       </div>
       <SectionWhyVegan />
-      <Category />
+      <section className={categoriesStyle["category"]}>
+        <div className="container">
+          <h2 className={categoriesStyle["title"]}>Categories</h2>
+          <ul className={categoriesStyle["category-list"]}>
+            {categories?.map((category) => {
+              return (
+                <Category
+                  path={category.path}
+                  name={category.name}
+                  thumb={category.thumb}
+                />
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+
       <Footer />
     </>
   );
